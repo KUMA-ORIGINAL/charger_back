c9ea16e7b10983e0389c1ebf999806df1f22cb91
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-12-31 16:43:15 +0600

    init

--- app/account/ocpp_routing.py
+++ app/account/ocpp_routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/ocpp/(?P<cp_id>\w+)/$', consumers.OCPPConsumer.as_asgi()),
+    re_path(r'ws/ocpp/(?P<cp_id>\w+)$', consumers.OCPPConsumer.as_asgi()),
 ]
