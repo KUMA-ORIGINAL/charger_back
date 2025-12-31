e1109c0212ea43cdbcb6e95b9229c6690b20ba1f
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-12-31 17:22:44 +0600

    init

--- app/config/asgi.py
+++ app/config/asgi.py
@@ -1,29 +1,28 @@
 """
 ASGI config for config project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
 """
 
 import os
+
+# 🔥 ВАЖНО: сначала settings
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+import django
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from channels.security.websocket import AllowedHostsOriginValidator
 
-# Импорт OCPP роутов
+# ✅ И ТОЛЬКО ПОТОМ импорт routing
 from account.ocpp_routing import websocket_urlpatterns
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
 django_asgi_app = get_asgi_application()
-
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket":
-        AuthMiddlewareStack(
-            URLRouter(websocket_urlpatterns)
-        )
-
+    "websocket": AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
+    ),
 })
